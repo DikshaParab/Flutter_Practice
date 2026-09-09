bce64a6facc11b24dd857da496a22526e0642b23
Diksha <dikshaparab777@gmail.com>
2026-09-09 17:00:53 +0530

    loan subtype dropdown added

---
+++ lib/widgets/bank/loan/loan_subtype_dd.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class LoanSubtypeDd extends StatelessWidget{
+  const LoanSubtypeDd({
+    super.key,
+    required this.value,
+    required this.items,
+    required this.onChanged,
+  });
+
+  final String? value;
+  final List<String> items;
+  final ValueChanged<String?> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Loan Sub-Type',
+          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 14),
+        ),
+        const SizedBox(height: 6,),
+        DropdownButtonFormField<String>(
+          initialValue: value, 
+          isExpanded: true,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            isDense: true,
+            contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+          ), items: items.map((v) => DropdownMenuItem(value: v, child: Text(v))).toList(),
+          onChanged: onChanged,
+        )
+      ],
+    );
+  }
+}

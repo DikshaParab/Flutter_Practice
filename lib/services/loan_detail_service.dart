b7edf75e5e88af8d5f76dbf5a21cd95bcf6b8fde
Diksha <dikshaparab777@gmail.com>
2026-09-09 17:02:46 +0530

    loan detail service updated

--- lib/services/loan_detail_service.dart
+++ lib/services/loan_detail_service.dart
@@ -1,26 +1,33 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'loan_edit_storage.dart';
 
 class LoanDetailService {
   static List<Map<String, dynamic>>? _cache;
 
   static Future<List<Map<String, dynamic>>> loadLoanDetails() async {
-    if (_cache != null) return _cache!;
-
-    final jsonString = await rootBundle.loadString(
-      'assets/data/loan_data.json',
-    );
-
+    final jsonString = await rootBundle.loadString('assets/data/customers.json');
     final data = jsonDecode(jsonString) as Map<String, dynamic>;
     final list = data['customers'] as List<dynamic>;
 
-    _cache = list.map((item) {
-      final customer = Map<String, dynamic>.from(item as Map<String, dynamic>);
-      final loan = customer['loan'] as Map<String, dynamic>;
-      customer['parsedDate'] = DateTime.parse(loan['date'] as String);
-      return customer;
-    }).toList();
+    final customers = <Map<String, dynamic>>[];
+    for (final item in list) {
+      var customer = Map<String, dynamic>.from(item as Map<String, dynamic>);
 
-    return _cache!;
+      final edit = await LoanEditStorage.getEdit(customer['customer_id'] as String);
+      if (edit != null) {
+        customer = {...customer, ...edit};
+      }
+
+      customer['parsedDate'] = DateTime.parse(customer['date'] as String);
+      customers.add(customer);
+    }
+
+    _cache = customers;
+    return customers;
+  }
+
+  static void clearCache() {
+    _cache = null;
   }
 }
